6a1e2c23d4149686cabaf5d74103ebfa8dd369fe
Linda Fernández <ada_hack153@hackademy.mx>
2023-01-18 18:35:52 -0600

    Botón Login y Registro en el Home
    Agregué el botón de login y registro en home

--- mainprojectq/src/main/java/sistema/clubgui/frmcoach.java
+++ mainprojectq/src/main/java/sistema/clubgui/frmcoach.java
@@ -30,7 +30,10 @@ public class frmcoach extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
+        botonLogin = new javax.swing.JPanel();
+        TextoLogin = new javax.swing.JLabel();
+        CajaRegistrar1 = new javax.swing.JPanel();
+        TextoRegistrar1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -42,13 +45,16 @@ public class frmcoach extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Open Sans Semibold", 0, 24)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("¡Bienvenido al sistema Coach!");
+        jLabel2.setText("¡Bienvenido al sistema!");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 570, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 709, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -72,8 +78,79 @@ public class frmcoach extends javax.swing.JFrame {
 
         bg.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 430, 710, 20));
 
-        jLabel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        bg.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 140, 80));
+        botonLogin.setBackground(new java.awt.Color(47, 106, 135));
+
+        TextoLogin.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        TextoLogin.setForeground(new java.awt.Color(255, 255, 255));
+        TextoLogin.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        TextoLogin.setText("Iniciar sesión ");
+        TextoLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        TextoLogin.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TextoLoginMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                TextoLoginMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                TextoLoginMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout botonLoginLayout = new javax.swing.GroupLayout(botonLogin);
+        botonLogin.setLayout(botonLoginLayout);
+        botonLoginLayout.setHorizontalGroup(
+            botonLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, botonLoginLayout.createSequentialGroup()
+                .addContainerGap(16, Short.MAX_VALUE)
+                .addComponent(TextoLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        botonLoginLayout.setVerticalGroup(
+            botonLoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, botonLoginLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(TextoLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        bg.add(botonLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 220, 160, 50));
+
+        CajaRegistrar1.setBackground(new java.awt.Color(47, 106, 135));
+
+        TextoRegistrar1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        TextoRegistrar1.setForeground(new java.awt.Color(255, 255, 255));
+        TextoRegistrar1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        TextoRegistrar1.setText("Registrarse");
+        TextoRegistrar1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        TextoRegistrar1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TextoRegistrar1MouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                TextoRegistrar1MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                TextoRegistrar1MouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout CajaRegistrar1Layout = new javax.swing.GroupLayout(CajaRegistrar1);
+        CajaRegistrar1.setLayout(CajaRegistrar1Layout);
+        CajaRegistrar1Layout.setHorizontalGroup(
+            CajaRegistrar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(CajaRegistrar1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(TextoRegistrar1, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(16, Short.MAX_VALUE))
+        );
+        CajaRegistrar1Layout.setVerticalGroup(
+            CajaRegistrar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, CajaRegistrar1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(TextoRegistrar1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        bg.add(CajaRegistrar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 220, 160, 50));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -88,6 +165,60 @@ public class frmcoach extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TextoLoginMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoLoginMouseClicked
+        frmlogin frmlogin = new frmlogin();
+        frmlogin.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_TextoLoginMouseClicked
+
+    private void TextoLoginMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoLoginMouseEntered
+        TextoLogin.setBackground(new Color (0, 156, 223));
+
+        Connection con=null;
+        try{
+            con=conexionbd();
+            ps=con.prepareStatement("INSERT INTO asesor(idCoach, nombre, uduario, password, correo) VALUES (?, ?, ?, ?, ?)");
+            ps.setString(1,CajaID.getText());
+            ps.setString(2,CajaNombre.getText());
+            ps.setString(3,CajaUsuario.getText());
+            ps.setString(4,CajaContraseña.getText());
+            ps.setString(5,CajaCorreo.getText());
+
+            int res=ps.executeUpdate();
+
+            if (res>0) {
+                JOptionPane.showMessageDialog(null, "Registro exitoso");
+                limpiarCajas();
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Error al registrar");
+                limpiarCajas();
+            }
+            con.close();
+        }catch(Exception e){
+            System.out.print(e.getMessage());
+        }
+
+    }//GEN-LAST:event_TextoLoginMouseEntered
+
+    private void TextoLoginMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoLoginMouseExited
+        TextoLogin.setBackground(new Color (47, 106, 135));
+    }//GEN-LAST:event_TextoLoginMouseExited
+
+    private void TextoRegistrar1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoRegistrar1MouseClicked
+        frmregistro frmregistro = new frmregistro();
+        frmregistro.setVisible(true);
+        this.dispose(); // TODO add your handling code here:
+    }//GEN-LAST:event_TextoRegistrar1MouseClicked
+
+    private void TextoRegistrar1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoRegistrar1MouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TextoRegistrar1MouseEntered
+
+    private void TextoRegistrar1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TextoRegistrar1MouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TextoRegistrar1MouseExited
 
     /**
      * @param args the command line arguments
@@ -125,9 +256,12 @@ public class frmcoach extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel CajaRegistrar1;
+    private javax.swing.JLabel TextoLogin;
+    private javax.swing.JLabel TextoRegistrar1;
     private javax.swing.JPanel bg;
+    private javax.swing.JPanel botonLogin;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
